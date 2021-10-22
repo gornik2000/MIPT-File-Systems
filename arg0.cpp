5656a6a21fb3fbc5b7707e7a8850c1c619abdab7
gornik2000 <gornik2000@mail.ru>
2021-10-22 23:03:39 +0300

    Solved tabulation issues

--- arg0.cpp
+++ arg0.cpp
@@ -3,14 +3,14 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2)
-	{
-		printf("ERROR! Try more arguements!\n");
-		return -1;
-	}
+  if (argc < 2)
+  {
+    printf("ERROR! Try more arguements!\n");
+    return -1;
+  }
 
-	prctl(PR_SET_MM, PR_SET_MM_ARG_START, argv[1], 0, 0);
-	FILE* cmd = fopen("/proc/self/cmdline", "r");
+  prctl(PR_SET_MM, PR_SET_MM_ARG_START, argv[1], 0, 0);
+  FILE* cmd = fopen("/proc/self/cmdline", "r");
   char test[10]{0};
   fscanf(cmd, "%10s", test);
   fclose(cmd);
@@ -18,5 +18,5 @@ int main(int argc, char* argv[])
   printf("In argv    = %s\n", argv[0]);
   printf("In cmdline = %s\n", test);
 
-	return 0;
+  return 0;
 }
